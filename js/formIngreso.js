d0097b391672a3a128215300a1a877138ae985bf
Emanuel Escudero <escudero.mdz@gmail.com>
2022-12-06 02:59:17 -0300

    Formularios de Registro e Ingreso con Funciones Declaradas, Funciones Flecha, Validación, Operador Ternario, Ciclos, Iteraciones, Condicionales, Objetos, Arrays, Storage

--- js/formIngreso.js
+++ js/formIngreso.js
@@ -1,47 +1,49 @@
 /* ======= FORMULARIO INGRESO ======= */
+function formEntrar() {
+  const emailInputLogin = document.getElementById("emailInputLogin");
+  const passInputLogin = document.getElementById("passInputLogin");
+  const alertLogin = document.getElementById("alertLogin");
+  const userText = document.getElementById("userText");
+  userText.innerHTML = localStorage.getItem("emailStorage");
 
-let emailInputLogin = document.getElementById("emailInputLogin");
-let passInputLogin = document.getElementById("passInputLogin");
-let alertLogin = document.getElementById("alertLogin");
+  /* BOTÓN "ENTRAR" */
+  const btnSubmitLogin = document.getElementById("btnSubmitLogin");
+  btnSubmitLogin.addEventListener("click", login); //Evento que dispara la función del form */
 
+  /* BOTÓN "ENTRAR": Función Buscar Si Existe la Cuenta */
+  function buscarCuenta(emailConsultado) {
+    return cuentas.find((elemento) => {
+      return elemento.email == emailConsultado;
+    });
+  }
 
-/* BOTÓN "ENTRAR" */
-let btnSubmitLogin = document.getElementById("btnSubmitLogin");
+  // Limpiar Forms:
+  function limpiarForms() {
+    forms.forEach((form) => {
+      form.reset();
+    });
+  }
 
-/* BOTÓN "ENTRAR": Evento que dispara la función del form */
-btnSubmitLogin.addEventListener("click", login);
+  /* BOTÓN "ENTRAR": Función que cierra el form si el ingreso es exitoso*/
+  function cerrarFormAlIngresar() {
+    tabContainer.classList.remove("active");
+  }
 
-/* BOTÓN "ENTRAR": Función que cierra el form si el ingreso es exitoso*/
-function cerrarFormAlIngresar() {
-  tabContainer.classList.remove("active");
-}
-
-/* BOTÓN "ENTRAR": Función que busca en el array cuentas[], una cuenta asociada mediante el email ingresado (Nota: Agregar comprobación de contraseña*/
-function login(event) {
-  event.preventDefault();
-  let emailConsultado = emailInputLogin.value;
-  let passConsultada = passInputLogin.value;
-  cuenta = buscarCuenta(emailConsultado);
-  if (cuenta != undefined && passConsultada == cuenta.pass) {
-    alertLogin.innerHTML = "✅ Ingreso Exitoso";
-    setTimeout(cerrarFormAlIngresar, 2000);
-    userText.innerHTML = cuenta.email;
-  } else {
-    alertLogin.innerHTML = "🚫 Usuario No Registrado o Datos Incorrectos";
-    limpiarForms();
+  /* BOTÓN "ENTRAR": Función que busca en el array cuentas[], una cuenta asociada mediante el email ingresado*/
+  function login(event) {
+    event.preventDefault();
+    let emailConsultado = emailInputLogin.value;
+    let passConsultada = passInputLogin.value;
+    cuenta = buscarCuenta(emailConsultado);
+    if (cuenta != undefined && passConsultada == cuenta.pass) {
+      alertLogin.innerHTML = "✅ Ingreso Exitoso";
+      localStorage.setItem("emailStorage", cuenta.email);
+      userText.innerHTML = localStorage.getItem("emailStorage");
+      setTimeout(cerrarFormAlIngresar, 2000);
+    } else {
+      alertLogin.innerHTML = "🚫 Usuario No Registrado o Datos Incorrectos";
+      limpiarForms();
+    }
   }
 }
-
-/* BOTÓN "ENTRAR": Función Buscar Si Existe la Cuenta */
-function buscarCuenta(emailConsultado) {
-  return cuentas.find((elemento) => {
-    return elemento.email == emailConsultado;
-  });
-}
-
-// Limpiar Forms:
-function limpiarForms() {
-  forms.forEach((form) => {
-    form.reset();
-  });
-}
+formEntrar();
