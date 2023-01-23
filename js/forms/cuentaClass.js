8adfdd3ed801bb656405f90950aba28c260bab3f
Emanuel Escudero <escudero.mdz@gmail.com>
2023-01-23 01:48:02 -0300

    Proyecto Final JavaScript CoderHouse - Escudero Emanuel

--- js/forms/cuentaClass.js
+++ js/forms/cuentaClass.js
@@ -1,10 +1,9 @@
 class Cuenta {
-  constructor(name, lastname, email, pass, cel, termsConditions) {
+  constructor(name, lastname, email, pass, cel) {
     this.name = name;
     this.lastname = lastname;
     this.email = email;
     this.pass = pass;
     this.cel = cel;
-    this.termsConditions = true;
   }
 }
